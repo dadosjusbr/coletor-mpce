66c393d0c95614d7bc98b9aacffdbcdc73a84a67
marcos-vas <marcosvaas@gmail.com>
2021-11-29 08:34:15 -0300

    fix: metadados

---
+++ src/metadado.py
@@ -0,0 +1,19 @@
+from coleta import coleta_pb2 as Coleta
+
+
+def captura(month, year):
+    metadado = Coleta.Metadados()
+    metadado.nao_requer_login = True
+    metadado.nao_requer_captcha = True
+    metadado.acesso = Coleta.Metadados.FormaDeAcesso.RASPAGEM_DIFICULTADA
+    metadado.extensao = Coleta.Metadados.Extensao.HTML
+    metadado.estritamente_tabular = True
+    metadado.formato_consistente = True
+    metadado.tem_matricula = True
+    metadado.tem_lotacao = True
+    metadado.tem_cargo = True
+    metadado.despesas = Coleta.Metadados.OpcoesDetalhamento.DETALHADO
+    metadado.receita_base = Coleta.Metadados.OpcoesDetalhamento.DETALHADO
+    metadado.outras_receitas = Coleta.Metadados.OpcoesDetalhamento.DETALHADO
+
+    return metadado
